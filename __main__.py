f423f492ac11cfb9b0e4a444c11561226a7cf24f
AmirAvenger <amiravenger1@outlook.com>
2024-06-09 18:48:17 +0330

    edit 1

--- __main__.py
+++ __main__.py
@@ -5,9 +5,9 @@ plugins = dict(root="plugins")
 app = Client(
     name="Translator",
     plugins=plugins,
-    api_id=28933710,
-    api_hash="31afd6e672be2836c266320601105119",
-    bot_token="7381662296:AAGOoWs62imyeTtO607sLIkpu59vnpBUWpg"
+    api_id=,
+    api_hash="",
+    bot_token=""
 )
 
 
